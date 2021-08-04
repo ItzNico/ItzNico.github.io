1002ed503192805efaeedf6c72f4d8011c17ca2d
ItzNico <47154062+ItzNico@users.noreply.github.com>
2021-08-03 23:59:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector('img');
+var audio = new Audio("images/tuki.mp3");
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/mirai.jpg') {
+      myImage.setAttribute('src','images/tuki.png');
+      audio.play();
+    } else {
+      myImage.setAttribute('src','images/mirai.jpg');
+      audio.play();
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt("What's your name? :3");
+    if(!myName) {
+        setUserName();
+    } else{
+        localStorage.setItem('name', myName);
+    myHeading.textContent = '"I love Mirai Kuriyama" -' + myName;
+    }
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = '"I love Mirai Kuriyama" -' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
